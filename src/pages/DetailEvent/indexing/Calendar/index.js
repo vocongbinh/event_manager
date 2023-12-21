54eb9b73bd1252a22abf53d826a1856ddf8d7880
vocongbinh <binh93phudong@gmail.com>
2023-12-21 10:45:30 +0700

    fix expired event

--- src/pages/DetailEvent/indexing/Calendar/index.js
+++ src/pages/DetailEvent/indexing/Calendar/index.js
@@ -4,9 +4,11 @@ import classNames from 'classnames/bind';
 import Button from '../../../../components/layouts/components/Button';
 import { format } from 'date-fns';
 import { useAuthContext } from '../../../../utils/authContext';
+import { useNavigate } from 'react-router-dom';
 const Calendar = forwardRef(({ data }, ref) => {
     const cx = classNames.bind(styles);
     const authContext = useAuthContext();
+    const navigate = useNavigate();
     return (
         <div ref={ref} className={cx('wrapper')}>
             <h1> calendar</h1>
@@ -16,6 +18,20 @@ const Calendar = forwardRef(({ data }, ref) => {
                 const dateEnd = format(new Date(item.endAt), 'EEEE, dd MMMM yyyy');
                 const timeStart = format(new Date(item.startAt), 'hh:mm aa');
                 const timeEnd = format(new Date(item.endAt), 'hh:mm aa');
+                let status;
+                if (timeStart < new Date()) {
+                    status = {
+                        content: 'Event is over',
+                        onClick: () => {},
+                    };
+                } else
+                    status = {
+                        content: 'Book now',
+                        onClick: () => {
+                            let path = authContext.getUser() ? `book/${item._id}/step1` : '/auth/login';
+                            navigate(path);
+                        },
+                    };
                 return (
                     <div className={`row ${cx('item')}`}>
                         <div className={`col-sm-9 col-12 ${cx('information')}`}>
@@ -24,12 +40,14 @@ const Calendar = forwardRef(({ data }, ref) => {
                         </div>
                         <div className="col-sm-3 col-12">
                             <Button
-                                href={authContext.getUser() ? `book/${item._id}/step1` : '/auth/login'}
+                                onClick={status.onClick}
                                 type="highlight"
                                 size="max"
-                                className={cx('book-btn')}
+                                className={cx('book-btn', {
+                                    expired: status.content === 'Event is over',
+                                })}
                             >
-                                Book now
+                                {status.content}
                             </Button>
                         </div>
                     </div>
